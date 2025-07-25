7993c93a50ca5135dbb0d84187d575f8b02e4d89
KunalSingh72 <kunalsiinghrathore@gmail.com>
2025-07-25 12:15:04 +0530

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/SGCSC/'>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
