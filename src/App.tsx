a17f095643a1df1dde1125c639af8136f8380999
KunalSingh72 <kunalsiinghrathore@gmail.com>
2025-07-25 11:13:12 +0530

    Copy Paste

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Routes, Route } from "react-router-dom";
+import Homepage from './pages/HomePage';
+import MainLayout from "./layout/MainLayout";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
+import Courses from "./pages/Courses";
+import Student from "./pages/Student";
+import EnrollmentVerification from "./components/studentPage/EnrollmentVerification";
+import ResultVerification from "./components/studentPage/ResultVerification";
+import CertificateVerification from "./components/studentPage/CertificateVerification";
+import AdmitCard from "./components/studentPage/AdmitCard";
+import StudentLogin from "./components/studentPage/StudentLogin";
+import Franchise from "./pages/Franchise";
+import FranchiseRegister from "./components/franchisePage/FranchiseRegister";
+
+
+
 function App() {
 
   return (
-    <>
-      Base URL
-      <img src={`${import.meta.env.BASE_URL}eleceed.jpeg`}></img>
-      
-    </>
+    <QueryClientProvider client={new QueryClient()}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="about" element={<About />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="student" element={<Student />} />
+          <Route path="/student/enrollment-verification" element={<EnrollmentVerification />} />
+          <Route path="/student/result-verification" element={<ResultVerification />} />
+          <Route path="/student/certificate-verification" element={<CertificateVerification />} />
+          <Route path="/student/admit-card" element={<AdmitCard />} />
+          <Route path="/student/login" element={<StudentLogin />} />
+          <Route path="franchise" element={<Franchise />} />
+          <Route path="franchise/register" element={<FranchiseRegister />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="gallery" element={<Gallery />} />
+
+        </Route>  
+      </Routes>
+    </QueryClientProvider>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import HeroSection from "../components/homePage/HeroSection";
+import CentersSection from "../components/homePage/CentersSection";
+
+export default function Homepage() {
+  return (
+    <>
+      <HeroSection />
+      <CentersSection />
+    </>
+  );
+}
